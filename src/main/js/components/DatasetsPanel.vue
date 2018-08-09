40e2ddd54547d58174a4dbc515eac393f641fa7f
Mohamed Gaily <mgaily@harmonia.com>
2018-08-09 15:47:56 -0400

    Created datasets panel.

---
+++ src/main/js/components/DatasetsPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="data-set-panel">
+        <el-table ref="multipleTable" :data="datasets" style="width: 100%" stripe @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55"/>
+            <el-table-column property="name" label="Name" width="120">
+                <template slot-scope="scope">
+                    <strong>{{ scope.row.name }}</strong>
+                    <br/>
+                    <small>{{ scope.row.description }}</small>
+                </template>
+            </el-table-column>
+    </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        datasets: function() {
+            return this.$store.getters.datasets;
+        }
+    },
+    methods: {
+        toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
+    }
+};
+</script>
--- src/main/js/stores/Stores.js
+++ src/main/js/stores/Stores.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import DatasetsStore from './DatasetsStore.js';
 
 Vue.use(Vuex);
 
 var store = new Vuex.Store({
    modules: {
+       datasetsStore: DatasetsStore
    }
 });
 

e133dd309bd4ae69c84e96c3290d353626da7640
Jonathan Foley <jfoley@harmonia.com>
2018-08-09 15:39:58 -0400

    Header and Nav-Bar
    *Created LogoBanner component containing the Header and Nav Bar.
    *Removed files that should not be tracked from index.

---
+++ src/main/js/components/LogoBanner.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="header-container">
+        <div class="logo-banner">
+            <div class="headerListDiv">
+                <ul>
+                    <li><a>LOGIN</a></li>
+                    <li><a>MY PROFILE</a></li>
+                    <li><a>CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav-bar">
+            <ul>
+                <li><a>DASHBOARD</a></li>
+                <li><a>NEW WORK</a></li>
+                <li><a>MY WORK</a></li>
+                <li><a>ANALYTICS and VISUALIZATION LIBRARY</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style>
+.logo-banner {
+    position: absolute;
+    top: 10px;
+    left: 0px;
+    right: 0px;
+    height: 82px;
+    background-image: url("../../webapp/js/logo.png");
+    background-repeat: no-repeat;
+    background-position: top center;
+    overflow: hidden;
+}
+.headerListDiv {
+    position:relative;
+    float:right;
+    margin-right:280px;
+    margin-top:49px;
+}
+
+.headerListDiv ul{
+    list-style-type: none;
+    overflow:hidden;
+}
+
+.headerListDiv li{
+    padding-right: 20px;
+    float:left;
+}
+
+.nav-bar {
+    position:absolute;
+    top: 92px;
+    height: 50px;
+    background-color: blue;
+    width:100%;
+}
+
+.nav-bar ul{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+}
+
+.nav-bar li {
+    float: left;
+}
+
+.nav-bar a {
+    display: block;
+    color:rgb(206, 200, 200);
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+}
+
+.nav-bar a:hover {
+    color:white;
+}
+
+#header-container a:hover {
+    cursor: pointer;
+}
+</style>
--- src/main/js/components/MainContainer.vue
+++ src/main/js/components/MainContainer.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        <p>Main Container</p>
+        <logo-banner />
+        <div class="main-container">
+        </div>
     </div>
 </template>
 
 <script>
-
+import LogoBanner from './LogoBanner.vue'
 export default {
     components: {
+        LogoBanner
     },
     mounted() {
 
@@ -16,6 +19,5 @@ export default {
     }
 }
 </script>
-
 <style>
 </style>

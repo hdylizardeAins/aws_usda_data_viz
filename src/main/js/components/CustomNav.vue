dee7b57e3c08598e1342140e90837fd3e00a1076
Jonathan Foley <jfoley@harmonia.com>
2018-08-10 01:08:47 -0400

    Used element-ui for header.
    *Header redone with element-ui.
    *Added nav-bar with basic functionality to show the page under construction when not on New Work tab.
    *Added index.html which is included in the web.xml descriptor so you don't need webpack server to use the app.
    *Created images subdirectory in src/main/webapp/js and ensured changes are tracked.

---
+++ src/main/js/components/CustomNav.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="menuDiv">
+      <el-menu
+      :default-active="activeIndex"
+      class="nav-menu"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#047cc0"
+      text-color="rgba(255, 255, 255, 0.842)"
+      active-text-color="#fff">
+        <el-menu-item index="1">DASHBOARD</el-menu-item>
+        <el-menu-item index="2">NEW WORK</el-menu-item>
+        <el-menu-item index="3">MY WORK</el-menu-item>
+        <el-menu-item index="4">ANALYTICS and VISUALIZATION LIBRARY</el-menu-item>
+        <el-menu-item index="5"><img src="../../webapp/js/images/question.png"></el-menu-item>
+      </el-menu>
+    </div>    
+</template>
+<script>
+export default {
+    data() {
+      return {
+        activeIndex: '2'
+      }
+    },
+    methods: {
+      handleSelect(key) {
+        this.$emit("nav-select", {
+          "key": key
+        });
+      }
+    }
+  }
+</script>
+<style>
+.nav-menu {
+  display: flex;
+  justify-content: center;
+}
+</style>

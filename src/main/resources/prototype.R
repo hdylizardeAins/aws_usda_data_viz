7e9a308cc3ae28806470fe648a6e6fc186e36089
Eric Wimberley <ewimberley@harmonia.com>
2018-08-10 15:25:55 -0400

    Image output

--- src/main/resources/prototype.R
+++ src/main/resources/prototype.R
@@ -22,17 +22,19 @@ if (length(args)==0) {
   #mydata <- read.csv("~/Documents/workspace-sts-3.9.5.RELEASE/aws_usda_data_viz/src/main/resources/geneticEngineeringAdoption.csv")
   mydata <- read.csv(inputFile)
   if(action == "plot"){
-    png(file = "myplot.png", bg = "transparent")
+    imgName <- as.character(length(list.files(outputDir)))
+    png(file = paste(outputDir, "/", imgName, ".png", collapse = "", sep = ""), bg = "transparent")
     plot(mydata)
-    dev.off()
+    invisible(dev.off())
+    cat(paste("{\"outputFile\":\"", imgName, ".png", "\"}", collapse = "", sep = ""))
   } else if(action == "summary"){
     summary(mydata)
   } else if(action == "regression"){
     linearMod <- lm(ValuePerAcre ~ USGECropPercentCorn, data=mydata)
     #print(linearMod)
-    cat(paste("{\"intercept\":", linearMod$coefficients[1], ",\"slope\":", linearMod$coefficients[2], "}"))
+    cat(paste("{\"intercept\":", linearMod$coefficients[1], ",\"slope\":", linearMod$coefficients[2], "}", collapse = "", sep = ""))
   } else if(action == "columns"){
     #print(colnames(mydata))
-    cat(paste("{\"columns\":[\"", paste(colnames(mydata), collapse = "\",\""), "\"]}"))
+    cat(paste("{\"columns\":[\"", paste(colnames(mydata), collapse = "\",\""), "\"]}", collapse = "", sep = ""))
   }
 }

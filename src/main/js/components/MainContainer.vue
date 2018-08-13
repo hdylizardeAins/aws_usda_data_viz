30ca6a52da8c74c21c2aa204543f0c3497de1070
Jonathan Foley <jfoley@harmonia.com>
2018-08-13 15:24:13 -0400

    Created Dataset Viewer for CSVs.
    *CsvViewer component with data table.
    *IngestCsv function for transforming csv strings into arrays.
    *Updated MainContainer -- further updates needed to make the CsvViewer show up when clicking a dataset icon.

--- src/main/js/components/MainContainer.vue
+++ src/main/js/components/MainContainer.vue
@@ -24,6 +24,7 @@
                 </el-main>
             </el-container>
         </el-container>
+        <csv-viewer :raw-data="datasetRawData" :showTable="showDatasetViewer"/>
     </div>
 </template>
 
@@ -33,6 +34,7 @@ import CustomHeader from './CustomHeader.vue';
 import CustomNav from './CustomNav.vue';
 import DatasetsPanel from './DatasetsPanel.vue';
 import Visualization from './Visualization.vue';
+import CsvViewer from './CsvViewer.vue';
 
 export default {
     components: {
@@ -40,11 +42,14 @@ export default {
         CustomNav,
         DatasetsPanel,
         AnalyticsPanel,
-        Visualization
+        Visualization,
+        CsvViewer
     },
     data(){
         return {
-            activeIndex: "2"
+            activeIndex: "2",
+            datasetRawData: "",
+            showDatasetViewer: false
         }
     },
     mounted() {

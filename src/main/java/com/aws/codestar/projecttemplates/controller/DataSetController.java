0132d2fb4a3095943e19c630bf3a5ac3284878c0
Mohamed Gaily <mgaily@harmonia.com>
2018-08-16 14:56:20 -0400

    Add name section to merge dialog

--- src/main/java/com/aws/codestar/projecttemplates/controller/DataSetController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/DataSetController.java
@@ -43,8 +43,6 @@ public class DataSetController {
 	private static final String PLOT_OUTPUT_DIR = "/var/www/html/";
 	private static final String CSV_EXTENSION = ".csv";
 	private static final String EXCEL_EXTENTION = ".xlsx";
-	private static final String GENETIC_ENGINEERING_ADOPTION_DISPLAY_PREFIX = "Genetic Engineering Adoption ";
-	private static final String GENETIC_ENGINEERING_ADOPTION_FILE_PREFIX = "geneticEngineeringAdoption-";
 	private static final String GENETIC_ENGINEERING_ADOPTION_FILE_EXT = CSV_EXTENSION;
 	private static final String DEFAULT_SHEET_NAME = "Data Sheet (machine readable)";
 
@@ -58,7 +56,8 @@ public class DataSetController {
 	 * @return the handle for the merged dataset
 	 */
 	@RequestMapping(path = "merge", method = RequestMethod.POST, consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
-	public ResponseEntity merge(@RequestBody List<FilteredDataSet> dataSets) {
+	public ResponseEntity merge(@RequestBody List<FilteredDataSet> dataSets, @RequestParam(value = "name") String mergedDatasetName) {
+		mergedDatasetName = mergedDatasetName.trim();
 		try {
 			RowSortedTable<String, String, String> finalGraph = TreeBasedTable.create();
 			String finalPivotColumn = null;
@@ -99,10 +98,10 @@ public class DataSetController {
 
 			StringBuilder generatedCSVFile = FileUtils.graphToCSV(finalPivotColumn, finalGraph);
 			long time = System.nanoTime();
-			String fileName = GENETIC_ENGINEERING_ADOPTION_FILE_PREFIX + time + GENETIC_ENGINEERING_ADOPTION_FILE_EXT;
+			String fileName = mergedDatasetName + "-" + time + GENETIC_ENGINEERING_ADOPTION_FILE_EXT;
 			String filePath = PLOT_OUTPUT_DIR + fileName;
 
-			String displayName = GENETIC_ENGINEERING_ADOPTION_DISPLAY_PREFIX.trim();
+			String displayName = mergedDatasetName;
 			File file = new File(filePath);
 			Files.asCharSink(file, Charsets.UTF_8).write(generatedCSVFile);
 			DataSet dataSet = new DataSet();
